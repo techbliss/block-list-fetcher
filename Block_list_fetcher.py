fb801b6623dd0b6ec8d2a054ae7b708042c6a190
Storm Shadow <Storm_Shadow@techbliss.org>
2016-04-19 18:39:28 +0200

    version 1.0 fixed http vs https

--- Block_list_fetcher.py
+++ Block_list_fetcher.py
@@ -96,8 +96,7 @@ class Ui_MainWindow(object):
         MainWindow.addToolBar(QtCore.Qt.LeftToolBarArea, self.toolBar_3)
         MainWindow.insertToolBarBreak(self.toolBar_3)
 
-        self.progress = QtGui.QProgressBar(self)
-        self.progress.setGeometry(200, 80, 250, 20)
+
 
         #populate action1
         #first action 1
